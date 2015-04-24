c7fdf8a8d250d02f5df7180dcb3dc2012bdfc146
Jessica Claire Edwards <jessica@eml.cc>
2015-04-24 14:51:58 +1000

    Updated transactions class.

--- main.rb
+++ main.rb
@@ -2,9 +2,14 @@ require 'rubygems'
 require 'pg'
 require './classes/database.rb'
 require './classes/product.rb'
+require './classes/transaction.rb'
 
 Database.connection( dbname: 'supermart' )
 
 puts Product.all
 
 puts Product.read(1)
+
+puts Transaction.all
+
+puts Transaction.create
