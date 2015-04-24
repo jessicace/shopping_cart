65774263ce0d31dca308ade35235c1e543a51404
Jessica Claire Edwards <jessica@eml.cc>
2015-04-24 10:46:56 +1000

    Created self.all method to return hash of items in database.

--- classes/product.rb
+++ classes/product.rb
@@ -1,8 +1,29 @@
 class Product < Database
   TABLE_NAME = "products"
+  attr_reader :id, :name, :price, :gst, :category
 
-  def initialize(id)
+  def initialize(id, name, price, gst, category)
     @id = id
+    @name = name
+    @price = price
+    @gst = gst
+    @category = category
+  end
+
+  def self.all
+    res = connection.exec("SELECT * FROM #{TABLE_NAME}")
+    hash = {}
+    res.each do |row|
+      hash[row['id']] = self.new(row['id'], row['name'], row['price'], row['gst'], row['category'])
+    end
+    hash
+  end
+
+  def self.read
+
   end
   
+  def to_s
+    "#{@name.capitalize} : $#{@price} (GST: #{@gst}) #{@id} Category: #{@category.capitalize}"
+  end
 end
